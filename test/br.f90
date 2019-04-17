0b1b29e50d16916289509986fadda3a97a6ffb37
Valentin Clement <clementval@gmail.com>
2019-04-17 09:16:25 +0200

    Add test data

---
+++ test/br.f90
@@ -0,0 +1,15 @@
+module br_test
+
+contains
+  subroutine sub1()
+    real :: a, b, c
+    integer :: d, e
+  
+    c = sin(a)
+    b = c ** a
+
+    e = ishift(d)
+
+  end subroutine sub1
+
+end module br_test
